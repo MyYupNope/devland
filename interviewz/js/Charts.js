183373c32e82a7789dd9203afccbe500a3973d67
RM <rodrigo.gc.matias@gmail.com>
2026-07-16 10:48:15 +0100

    chore: add version query suffix to State.js imports in app.js and Charts.js to bust module cache

--- interviewz/js/Charts.js
+++ interviewz/js/Charts.js
@@ -1,5 +1,5 @@
 import { parseDate } from './Utils.js';
-import { state } from './State.js';
+import { state } from './State.js?v=2';
 
 let cumulativeSubmissionsChartInstance = null;
 let statusSplitChartInstance = null;
--- interviewz/js/app.js
+++ interviewz/js/app.js
@@ -1,6 +1,6 @@
 import { FacetedSelect } from './FacetedSelect.js';
 import { FormApp } from './FormApp.js';
-import { state } from './State.js';
+import { state } from './State.js?v=2';
 import { resumeApp } from './Resume.js';
 import { renderAllDashboardWidgets } from './Charts.js';
 import { parseMarkdown } from './Markdown.js';
